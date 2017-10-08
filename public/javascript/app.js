1e87aa358bd72c9ea49f1e57f17b66b92eff2faa
Isaac Kwon <you@example.com>
2017-10-07 17:50:41 -0700

    Implemented handlebars

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,6 +2,25 @@ function createPost() {
   $("#create-post").on("click", () => {
     $("#modal").modal("show");
   });
+  $("#submit-btn").on("click", () => {
+    $("#modal").modal("hide");
+    userInput();
+  });
+}
+
+function userInput() {
+  const make = $("#make").val();
+  const model = $("#model").val();
+  const year = $("#year").val();
+  const mileage = $("#mileage").val();
+  const city = $("#city").val();
+  const bodyStyle = $(".radio-inline [name = 'optradio']:checked").val();
+  console.log(make);
+  console.log(model);
+  console.log(year);
+  console.log(mileage);
+  console.log(city);
+  console.log(bodyStyle);
 }
 
 createPost();
