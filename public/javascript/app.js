1d015646155e23b5a257f107d6db2cfa8488b281
Isaac Kwon <you@example.com>
2017-10-12 20:26:03 -0700

    detail modal fixed

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -28,17 +28,16 @@ function userInput() {
   console.log(bodyStyle);
 }
 
-function moreInfo() {
-  $(".btn-primary").on("click", () => {
-    $("#detail-modal").modal("show");
-    // $('#detail-modal').on('show.bs.modal', function(event) {
-    //   const button = $(event.relatedTarget)
-    //   const recipient = button.data("id");
-    //   const modal = $(this)
-    //   modal.find('.modal-title').text('New message to ' + recipient)
-    //   modal.find('.modal-body input').val(recipient)
-    // });
-  });
-}
-
-moreInfo();
+$('#detail-modal').on('show.bs.modal', function (event) {
+  const button = $(event.relatedTarget);
+  const make = button.data("make");
+  const model = button.data("model");
+  const contact = button.data("contact");
+  const transmission = button.data("transmission");
+  const description = button.data("description");
+  const modal = $(this)
+  modal.find('.modal-title').text(`More Information on ${make} ${model}`);
+  modal.find("#contact").text(`Contact Info: ${contact}`);
+  modal.find("#transmission").text(`Transmission Type: ${transmission}`);
+  modal.find("#description").text(`Description/Comments: ${description}`);
+})
