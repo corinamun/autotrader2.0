32d98151f8d30d210d560a4a0bfd096a6cc564b8
Corina Mun <corinamun@gmail.com>
2017-10-15 21:09:36 -0700

    css and testing

--- test/test.js
+++ test/test.js
@@ -1,16 +1,24 @@
 var expect = require("chai").expect;
 
+const Nightmare = require ('nightmare');
+
 // var Trade = require("trade_controllers");
 // var trade = Trade ({ show: true });
 
 describe("trade", function() {
-  it("should redirect page to /trades GET", function(trades) {
-    chai.request(server)
-      .get('/trades')
-      .end(function(err,res) {
-        res.should.have.status(200);
-        trades();
-      })
+  it("should redirect user to trades route", function(url){
+    var nightmare = Nightmare();
+    return nightmare.goto('http://localhost:3000').end().then(function(result) {
+      console.log(result);
+    });
+  })
+  // it("should redirect page to /trades GET", function(trades) {
+  //   chai.request(server)
+  //     .get('/trades')
+  //     .end(function(err,res) {
+  //       res.should.have.status(200);
+  //       trades();
+  //     })
   // it("should display html elements from hbs GET", function(hbs) {
   //   chai.request(server)
   //   .get('/trades.api')
@@ -30,4 +38,4 @@ describe("trade", function() {
   //   chai.request(server)
   //   .post('/trades')
   //   .end(function(err,res) {
-  // });
+   });
